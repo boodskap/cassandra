b8eecfa5815c8f854ccc2f4b72765e9ccb672a9d
Jegan Vincent <jvincent@boodskap.io>
2020-08-13 10:38:32 +0000

    removed db initialization, boodskap will init it on first start

--- initdb.sh
+++ initdb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Initialinging database...."
-/root/bin/cqlsh 10.1.1.3 -f /initks.cql
-/root/bin/cqlsh 10.1.1.3 -f /initdb.cql
+#/root/bin/cqlsh 10.1.1.3 -f /initks.cql
+#/root/bin/cqlsh 10.1.1.3 -f /initdb.cql
+echo "Boodskap platform will initialize the tables and indexes at the first startup"
