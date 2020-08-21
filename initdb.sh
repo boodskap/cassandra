691073958468af02aea326359b533929afc2582d
Jegan Vincent <jvincent@boodskap.io>
2020-08-21 11:23:03 +0000

    Compressed the container size

--- initdb.sh
+++ initdb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#/root/bin/cqlsh 10.1.1.3 -f /initks.cql
+/root/bin/cqlsh 10.1.1.3 -f /initks.cql
 #/root/bin/cqlsh 10.1.1.3 -f /initdb.cql
 echo "Boodskap platform will initialize the tables and indexes at the first startup"
